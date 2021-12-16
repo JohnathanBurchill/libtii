3012f877c3894395c1542e5edcc7185851553c9e
efi tomcat <tomcat@efi.phys.ucalgary.ca>
2021-12-16 00:06:31 -0700

    Install script to run tiim and make a mp4 movie

---
+++ Swarm_fast_movie_generator.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+(cd /tmp/TII-movies-simple;
+
+
+HDRFILE=$1
+BASE=`basename $HDRFILE`
+SAT=`echo $BASE | cut -c 12`
+DATE=`echo $BASE | cut -c 20-25`
+SCALE=$2
+
+MOVIEFILE=`tiim $1 $2`
+
+/usr/local/bin/ffmpeg -y -loglevel quiet -an -i EFI${SAT}_%05d.png  -s 640x480 -c:v libx264 -vf "fps=15,format=yuv420p" -profile:v baseline -level 3 /data/Movies/Swarm/${DATE}/${MOVIEFILE}
+
+echo "tiim'd backup movie ${MOVIEFILE}"
+
+rm EFI${SAT}_?????.png
+
+)
