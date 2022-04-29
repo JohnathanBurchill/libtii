099ff4ae750c6ac9bc3c90cd20d1d50b70b5fc49
Johnathan Burchill <j.burchill@ucalgary.ca>
2022-04-29 20:11:04 +0000

    Fixed argument name for tii_dump script

--- tools/tii_dump/parallel_process_tii_dump.sh
+++ tools/tii_dump/parallel_process_tii_dump.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# processes TII images to determine presence of anomalies for specified satellite for a range of dates
+# Dumps TII packet parameters to a file
 # http://stackoverflow.com/questions/28226229/how-to-loop-through-dates-using-bash
 
 if test "$#" -ne "6"; then
-  echo "Usage: $0 satelliteLetter gainMapId startDate stopDate numCpus outputDirectory"
+  echo "Usage: $0 satelliteLetter parameterIDs startDate stopDate numCpus outputDirectory"
   exit 1
 fi
 
 satellite=$1
-gainMapId=$2
+parameters=$2
 startDate=$3
 stopDate=$4
 numCpus=$5
@@ -22,7 +22,7 @@ cpuNum=0
 
 while [ $cpuNum -lt $numCpus ]; do
 	cpuNum=$((cpuNum + 1))
-	xterm -geometry 80x1 -e "process_tii_dump.sh ${satellite} ${gainMapId} ${dateToProcess} ${stopDate} $numCpus ${outDir}" &
+	xterm -geometry 80x1 -e "process_tii_dump.sh ${satellite} ${parameters} ${dateToProcess} ${stopDate} $numCpus ${outDir}" &
 	dateToProcess=$(date -I -d "$dateToProcess + 1 day")
 done
 
